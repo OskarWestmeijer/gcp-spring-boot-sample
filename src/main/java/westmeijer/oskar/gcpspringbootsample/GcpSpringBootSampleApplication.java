de33732575805377dca1960afc17fd82b5b722ca
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-03-25 18:13:47 +0100

    init

---
+++ src/main/java/westmeijer/oskar/gcpspringbootsample/GcpSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package westmeijer.oskar.gcpspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSpringBootSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/westmeijer/oskar/gcpspringbootsample/GcpSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package westmeijer.oskar.gcpspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
