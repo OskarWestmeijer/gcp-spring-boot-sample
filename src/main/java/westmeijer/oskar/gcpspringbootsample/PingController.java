6a409918b2cc05560179465ffe2842a5fa8615e1
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-06-04 20:38:33 +0200

    feat: min scale 1

--- src/main/java/westmeijer/oskar/gcpspringbootsample/PingController.java
+++ src/main/java/westmeijer/oskar/gcpspringbootsample/PingController.java
@@ -1,17 +1,24 @@
 package westmeijer.oskar.gcpspringbootsample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class PingController {
 
+    Logger log = LoggerFactory.getLogger(PingController.class);
+
     @GetMapping("/ping")
     public ResponseEntity<Map<String, String>> pingPong() {
-        return ResponseEntity.ok(Map.of("response", "pong"));
+        UUID trace = UUID.randomUUID();
+        log.info("Received ping request. Trace: {}", trace);
+        return ResponseEntity.ok(Map.of("response", "pong", "trace", trace.toString()));
     }
 
 }
