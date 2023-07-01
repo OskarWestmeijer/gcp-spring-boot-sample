3db65613507dd90da9132d57151f095df3804e77
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-01 21:53:06 +0200

    fix: terraform fmt

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_run_service" "gcp_spring_boot_sample" {
   name     = "gcp-spring-boot-sample"
   location = var.project_region
-  count = var.service_active
+  count    = var.service_active
 
   template {
     spec {
@@ -31,7 +31,7 @@ resource "google_cloud_run_service" "gcp_spring_boot_sample" {
 }
 
 resource "google_cloud_run_service_iam_binding" "service_access_control" {
-  count = length(google_cloud_run_service.gcp_spring_boot_sample)
+  count    = length(google_cloud_run_service.gcp_spring_boot_sample)
   location = google_cloud_run_service.gcp_spring_boot_sample[count.index].location
   service  = google_cloud_run_service.gcp_spring_boot_sample[count.index].name
   role     = "roles/run.invoker"
